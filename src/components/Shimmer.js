a14aa6cf74ce859ad22a522385192ad442831385
Saurav <91719965+saurav2942@users.noreply.github.com>
2023-12-11 17:17:25 +0530

    Update Shimmer.js

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -4,13 +4,16 @@ const Shimmer = () => {
   return (
     <div className="noise bg-[#fff] min-h-screen m-0 p-5">
       <div className="min-h-screen grid grid-cols-3 gap-2 md:grid-cols-4 md:gap-4 my-5 container mx-auto xl:px-20">
-        <div className="col-span-3 aspect-square md:aspect-auto md:row-span-2 bg-gray-200 rounded-3xl overflow-hidden"></div>
-        <div className="col-span-1 aspect-square bg-gray-200 rounded-3xl overflow-hidden"></div>
-        <div className="col-span-1 aspect-square bg-gray-200 rounded-3xl overflow-hidden"></div>
+        <div className="col-span-3 md:row-span-2 bg-gray-200 rounded-3xl overflow-hidden"></div>
+        <div className="col-span-1 bg-gray-200 rounded-3xl overflow-hidden"></div>
+        <div className="col-span-1 bg-gray-200 rounded-3xl overflow-hidden"></div>
 
-        <div className="col-span-1 aspect-square bg-gray-200 rounded-3xl overflow-hidden"></div>
-        <div className="col-span-1 aspect-square bg-gray-200 rounded-3xl overflow-hidden"></div>
+        <div className="col-span-1 bg-gray-200 rounded-3xl overflow-hidden"></div>
+        <div className="col-span-1 aspect-auto bg-gray-200 rounded-3xl overflow-hidden"></div>
         <div className="col-span-2 aspect-auto bg-gray-200 rounded-3xl overflow-hidden"></div>
+        <div className="col-span-2 aspect-auto bg-gray-200 rounded-3xl overflow-hidden"></div>
+        <div className="col-span-1 aspect-auto bg-gray-200 rounded-3xl overflow-hidden"></div>
+        <div className="col-span-3 md:col-span-1 aspect-auto bg-gray-200 rounded-3xl overflow-hidden"></div>
       </div>
     </div>
   );
