4122f9ed898374f4488bd51aff46377df7c96d84
Saurav <91719965+saurav2942@users.noreply.github.com>
2024-03-02 17:16:45 +0530

    Update Shimmer.js

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Shimmer = () => {
   return (
-    <div className="noise bg-[#fff] min-h-screen m-0 p-5">
+    <div className="noise bg-[#fff] min-h-screen m-0 p-5 animate-pulse">
       <div className="min-h-screen grid grid-cols-3 gap-2 md:grid-cols-4 md:gap-4 my-5 container mx-auto xl:px-20">
         <div className="col-span-3 md:row-span-2 bg-gray-200 rounded-3xl overflow-hidden"></div>
         <div className="col-span-1 bg-gray-200 rounded-3xl overflow-hidden"></div>
