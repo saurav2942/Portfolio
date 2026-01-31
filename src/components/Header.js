f608bc654028596a8094c42cff97aea5bf01e4be
Saurav Mathuriya <sauravmathuriya@gmail.com>
2026-01-31 10:27:35 +0530

    Again pick changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,10 +9,12 @@ const Header = () => {
   return (
     <div className="relative col-span-3 aspect-square md:aspect-auto md:row-span-2 bg-[#af72ff56] rounded-3xl overflow-hidden">
       <div className="h-full  absolute  p-4 md:p-8 flex flex-col justify-start  md:justify-end">
-        <img
-          className="w-32 h-32 mb-4 rounded-full aspect-square"
-          src={img}
-        ></img>
+        <div className="w-28 h-28 md:w-24 md:h-24 lg:w-40 lg:h-40 mb-4 rounded-full overflow-hidden shadow-[0_10px_30px_rgba(0,0,0,0.35)]">
+          <img
+            className="w-full h-full object-cover object-[50%_20%] scale-[1]"
+            src={img}
+          ></img>
+        </div>
         <h1 className="text-3xl md:text-xl  md:w-4/5 lg:text-5xl font-bold text-white mb-4 lg:mb-5">
           Hi, I'm Saurav Mathuriya
         </h1>
