c6b6b3f8bb18a77858325870d246f92b9c0c1f6d
Saurav Mathuriya <sauravmathuriya@gmail.com>
2026-01-31 12:32:48 +0530

    UI Changes Of Sizing

--- src/components/Body.js
+++ src/components/Body.js
@@ -18,66 +18,68 @@ import Project from "./Project";
 const Body = () => {
   return (
     <div className="bg-[#000] min-h-screen m-0 p-5">
-      <div className="grid grid-cols-3 gap-2 md:grid-cols-4 md:gap-4 my-5 container mx-auto xl:px-20">
-        {/* {Header} */}
-        <Header />
-        {/* Resume */}
-        <Link to="./resume">
-          <CvCard />
-        </Link>
-        {/* Linkedin  */}
-        <Social
-          URL={Linkedin_URL}
-          name="fa-linkedin-in"
-          type="brand"
-          bg="bg-[#2895fb56]"
-          col="#ffffff"
-        />
-        {/* Twitter  */}
-        <Social
-          // URL={Twitter_URL}
-          URL={Instagram_URL}
-          // name="fa-twitter"
-          name="fa-instagram"
-          type="brand"
-          bg="bg-[linear-gradient(45deg,_#f09433_0%,_#e6683c_25%,_#dc2743_50%,_#cc2366_75%,_#bc1888_100%)]"
-          col="#ffffff"
-          iconSize="text-[2.5rem] sm:text-[5.5rem] md:text-[6.5rem] lg:text-[7.5rem]"
-        />
-        {/* Medium  */}
-        <Social
-          URL={Medium_URL}
-          name="fa-medium-m"
-          type="brand"
-          bg="bg-[#ffffff]"
-          col="#171717"
-        />
-        {/* {Github} */}
-        <Github />
-        {/* {Projects} */}
-        <Link
-          to="./projects"
-          className="relative rounded-3xl col-span-2 md:aspect-auto overflow-hidden scale transition duration-300 ease-in-out hover:scale-105"
-        >
-          <Project />
-        </Link>
-        {/* Leetcode  */}
-        <Social
-          URL={Leetcode_URL}
-          name="leetcode"
-          type="leetcode"
-          col="#ffffff"
-          bg="bg-[#171717]"
-        />
-        {/* Codechef  */}
-        <Social
-          URL={Codechef_URL}
-          name="codechef"
-          type="codechef"
-          bg="bg-[#ffffff]"
-          col="#171717"
-          imgSize="w-[2.5rem] sm:w-[5.5rem] md:w-[6.5rem] lg:w-[7.5rem]"
-        />
+      <div className="pl-10 pr-10">
+        <div className="grid grid-cols-3 gap-2 md:grid-cols-4 md:gap-4 my-5 container mx-auto xl:px-20 pl-5 pr-5">
+          {/* {Header} */}
+          <Header />
+          {/* Resume */}
+          <Link to="./resume">
+            <CvCard />
+          </Link>
+          {/* Linkedin  */}
+          <Social
+            URL={Linkedin_URL}
+            name="fa-linkedin-in"
+            type="brand"
+            bg="bg-[#2895fb56]"
+            col="#ffffff"
+          />
+          {/* Twitter  */}
+          <Social
+            // URL={Twitter_URL}
+            URL={Instagram_URL}
+            // name="fa-twitter"
+            name="fa-instagram"
+            type="brand"
+            bg="bg-[linear-gradient(45deg,_#f09433_0%,_#e6683c_25%,_#dc2743_50%,_#cc2366_75%,_#bc1888_100%)]"
+            col="#ffffff"
+            iconSize="text-[2.5rem] sm:text-[5.5rem] md:text-[6.5rem] lg:text-[7.5rem]"
+          />
+          {/* Medium  */}
+          <Social
+            URL={Medium_URL}
+            name="fa-medium-m"
+            type="brand"
+            bg="bg-[#ffffff]"
+            col="#171717"
+          />
+          {/* {Github} */}
+          <Github />
+          {/* {Projects} */}
+          <Link
+            to="./projects"
+            className="relative rounded-3xl col-span-2 md:aspect-auto overflow-hidden scale transition duration-300 ease-in-out hover:scale-105"
+          >
+            <Project />
+          </Link>
+          {/* Leetcode  */}
+          <Social
+            URL={Leetcode_URL}
+            name="leetcode"
+            type="leetcode"
+            col="#ffffff"
+            bg="bg-[#171717]"
+          />
+          {/* Codechef  */}
+          <Social
+            URL={Codechef_URL}
+            name="codechef"
+            type="codechef"
+            bg="bg-[#ffffff]"
+            col="#171717"
+            imgSize="w-[2.5rem] sm:w-[5.5rem] md:w-[6.5rem] lg:w-[7.5rem]"
+          />
+        </div>
       </div>
       <Footer />
     </div>
