a32763be50f61d1e28a11506f72deff4c5336fa2
Saurav Mathuriya <sauravmathuriya@gmail.com>
2026-01-31 09:29:45 +0530

    Again Footer Changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,12 +5,15 @@ const Footer = () => {
     <div className="mt-auto bottom-0 z-50 flex flex-cols justify-center items-center text-white">
       <div className="w-screen">
         <hr className="my-6 border-gray-200 sm:mx-auto lg:my-4"></hr>
-        {/* <p className="flex justify-center text-sm text-white text-center my-4">
+        <p className="flex justify-center text-sm text-white text-center my-4">
           Designed by&nbsp;
-          <a href="https://enjae.netlify.app/" className="hover:underline">
-            enjae™
+          <a
+            href="https://www.linkedin.com/in/saurav2942/"
+            className="hover:underline"
+          >
+            Saurav™
           </a>
-        </p> */}
+        </p>
       </div>
     </div>
   );
